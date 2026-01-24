a1be3420dee2cbdc9ced85edecd38d0034127c6b
Lokesh <lokeshmanideep14@gmail.com>
2026-01-23 18:03:00 -0600

    added redirection to new website

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
-import Main from "./containers/Main";
-import { ThemeProvider } from "styled-components";
-import { chosenTheme } from "./theme";
-import { GlobalStyles } from "./global";
 
 function App() {
+  const newPortfolioUrl = "https://lokeshmanideep.github.io/";
+
+  useEffect(() => {
+    // Redirect to the new portfolio website
+    window.location.replace(newPortfolioUrl);
+  }, []);
+
   return (
-    <ThemeProvider theme={chosenTheme}>
-      <>
-        <GlobalStyles />
-        <div>
-          <Main theme={chosenTheme} />
-        </div>
-      </>
-    </ThemeProvider>
+    <div className="redirect-container">
+      <div className="redirect-content">
+        <h1>Portfolio Moved</h1>
+        <p>This portfolio has moved to a new location.</p>
+        <p>
+          If you are not redirected automatically, please click the link below:
+        </p>
+        <a href={newPortfolioUrl} className="redirect-link">
+          Visit New Portfolio →
+        </a>
+      </div>
+    </div>
   );
 }
 
